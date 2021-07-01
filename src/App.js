61ad40964170bf5686cf973de9bb9a43a7308904
Nagaveni <tsnagaveni62@gmail.com>
2021-07-01 22:23:55 +0530

    reset:

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
                     Free
                   </h5>
                   <h6 class="card-price text-center">
-                    $0<span class="period">/per month</span>
+                    $0<span class="period">/month</span>
                   </h6>
 
                   <hr />
